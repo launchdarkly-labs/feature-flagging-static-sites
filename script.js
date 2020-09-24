0dd1b0595ba586601f1bfed14d5b6b2f3640e542
Glitch (hello-webpage) <none>
2020-09-24 23:37:55 +0000

    💘👗 Checkpoint ./script.js:9618/538 ./index.html:9618/343

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-var user = {
-  "key": "aa0ceb"
-};
-var ldclient = window.LDClient.initialize('', user);
+const user = {"anonymous": true};
+const ldclient = window.LDClient.initialize('5e29e34e2a49f409996d7194', user);
+ldclient.on('ready', function(){
+  const main = document.querySelector('#main');
+  const isRedesignOn = ldclient.variation('redesign', false);
+  if (isRedesignOn) {
+    main.setAttribute('class', 'redesign');
+  } else {
+    main.
+  }
+});
