a8ba4bf50ef3f177f98697b353639389390a1c10
Glitch (hello-webpage) <none>
2020-09-24 23:25:56 +0000

    💸🌓 Checkpoint ./index.html:9618/21464 ./script.js:9618/312

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+var user = {
+  "key": "aa0ceb"
+};
+var ldclient = window.LDClient.initialize('', user);
