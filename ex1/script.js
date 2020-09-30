f888d2e26468a3332591519cc67be9479d83c0ce
Glitch (hello-webpage) <none>
2020-09-30 19:25:44 +0000

    🌁🏰 Checkpoint ./ex1/script.js:9618/1133

--- ex1/script.js
+++ ex1/script.js
@@ -10,7 +10,9 @@ const user = { anonymous: true };
 const ldclient = window.LDClient.initialize("5e29e34e2a49f409996d7194", user);
 ldclient.on("ready", function() {
   updateDesign(ldclient.variation("redesign", false));
-});
-ldclient.on("change:redesign", function(newVal, prevVal) {
-  updateDesign(newVal);
+  
+  ldclient.on("change:redesign", function(newVal, prevVal) {
+    debugger;
+    updateDesign(newVal);
+  });
 });
