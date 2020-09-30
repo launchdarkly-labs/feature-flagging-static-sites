40f1dc9d587145b3ba79178c0b5e809e951947bc
Glitch (hello-webpage) <none>
2020-09-30 19:35:42 +0000

    🍀💍 Checkpoint ./ex1/script.js:9618/325

--- ex1/script.js
+++ ex1/script.js
@@ -8,11 +8,11 @@ function updateDesign(isRedesignOn) {
 
 const user = { anonymous: true };
 const ldclient = window.LDClient.initialize("5e29e34e2a49f409996d7194", user);
+
 ldclient.on("ready", function() {
   updateDesign(ldclient.variation("redesign", false));
   
   ldclient.on("change:redesign", function(newVal, prevVal) {
-    debugger;
     updateDesign(newVal);
   });
 });
